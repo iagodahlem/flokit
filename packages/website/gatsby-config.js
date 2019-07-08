c257ff0dbd9505b32a48fd48042a10c4161f1f95
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2019-07-08 18:31:01 -0300

    Add website :earth-americas:

---
+++ packages/website/gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Flokit',
+    description: 'Toolkit for easily and smoothly building UIs with React.',
+    author: '@iagodahlem',
+    install: 'yarn add flokit',
+    github: 'https://github.com/iagodahlem/flokit',
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+  ],
+}
---
+++ packages/website/src/components/Emoji.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Emoji = ({ symbol, label }) => (
+  <span
+    className='emoji'
+    role='img'
+    aria-label={label}
+    aria-hidden={label ? 'false' : 'true'}
+  >
+    {symbol}
+  </span>
+)
+
+Emoji.propTypes = {
+  symbol: PropTypes.string.isRequired,
+  label: PropTypes.string,
+}
+
+Emoji.defaultProps = {
+  label: '',
+}
+
+export default Emoji
