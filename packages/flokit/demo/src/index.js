59c877614bbe00a213b5ea0f34a34fc3eab7705f
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2019-10-11 18:42:12 -0300

    Add sizes theme key on layout

--- packages/flokit/demo/src/index.js
+++ packages/flokit/demo/src/index.js
@@ -1,28 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
 import { Box, Flex } from '../../src'
 import './index.css'
 
+const theme = {}
+
 const Demo = () => (
-  <Box width={1} height='100%'>
-    <Flex height='100%' justifyContent='center' alignItems='center' bg='#fafafa'>
-      <h1>Flokit Playground</h1>
-    </Flex>
-
-    <Flex m='0 auto' p='1.8rem' maxWidth='780px'>
-      <Flex bg='red' alignItems='center' justifyContent='center' width={0.33}>
-        1
+  <ThemeProvider theme={theme}>
+    <Box width={1} height='100%'>
+      <Flex height='100%' justifyContent='center' alignItems='center' bg='#fafafa'>
+        <h1>Flokit Playground</h1>
       </Flex>
 
-      <Flex bg='blue' alignItems='center' justifyContent='center' width={0.33}>
-        2
-      </Flex>
+      <Flex m='0 auto' p='1.8rem' maxWidth='780px'>
+        <Flex bg='red' alignItems='center' justifyContent='center' width={0.33}>
+          1
+        </Flex>
 
-      <Flex bg='green' alignItems='center' justifyContent='center' width={0.33}>
-        3
+        <Flex bg='blue' alignItems='center' justifyContent='center' width={0.33}>
+          2
+        </Flex>
+
+        <Flex bg='green' alignItems='center' justifyContent='center' width={0.33}>
+          3
+        </Flex>
       </Flex>
-    </Flex>
-  </Box>
+    </Box>
+  </ThemeProvider>
 )
 
 ReactDOM.render(<Demo />, document.querySelector('#demo'))
--- packages/flokit/src/system/layout.js
+++ packages/flokit/src/system/layout.js
@@ -1,26 +1,60 @@
 import {
+  style,
+  num,
+  get,
+  px,
+  mapProps,
   compose,
   display,
-  width,
-  height,
-  size,
-  minWidth,
-  maxWidth,
-  minHeight,
-  maxHeight,
   verticalAlign,
   overflow,
 } from 'styled-system'
+
+const getWidth = (n, scale) => {
+  if (!num(n)) {
+    return px(get(scale, n, n))
+  }
+
+  if (n > 1) {
+    return px(n)
+  }
+
+  return `${n * 100}%`
+}
+
+const createSizeProp = prop => style({
+  prop,
+  transformValue: getWidth,
+  key: 'sizes',
+})
+
+const width = createSizeProp('width')
+const height = createSizeProp('height')
+const minWidth = createSizeProp('minWidth')
+const maxWidth = createSizeProp('maxWidth')
+const minHeight = createSizeProp('minHeight')
+const maxHeight = createSizeProp('maxHeight')
+
+const size = mapProps(props => ({
+  ...props,
+  width: props.size,
+  height: props.size,
+}))(
+  compose(
+    width,
+    height
+  )
+)
 
 export default compose(
   display,
   width,
   height,
-  size,
   minWidth,
   maxWidth,
   minHeight,
   maxHeight,
+  size,
   verticalAlign,
-  overflow,
+  overflow
 )
